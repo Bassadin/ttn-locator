11c46faba284e0ff4046f2084777a47cea24fef2
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-04 11:10:02 +0200

    add more env config values

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { ConfigModule } from '@nestjs/config';
         TypeOrmModule.forRoot({
             type: 'postgres',
             host: process.env.DATABASE_HOST,
-            port: 5432,
+            port: parseInt(process.env.DATABASE_PORT),
             username: process.env.DATABASE_USER,
             password: process.env.DATABASE_PASSWORD,
             database: process.env.DATABASE_NAME,
