7f7825a8fae28e115bbf8c7e591f00eaff9cf352
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-04 10:56:24 +0200

    add .env config with gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
         // https://docs.nestjs.com/techniques/database
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
+            host: process.env.DATABASE_HOST,
             port: 5432,
-            username: 'postgres',
-            password: 'postgres',
-            database: 'ttn-locator',
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
             entities: [TTNMapperMeasurement],
             synchronize: true,
         }),
