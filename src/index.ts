565045459a067bb5ff4356a38c43ac89c48c08a2
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-10 00:09:48 +0200

    edit app.use call to be in line with the rest

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ const pinoHttpLogger = pinoHttp();
 app.use(pinoHttpLogger);
 
 // Register Routes
-app.use(BaseRoutes);
+app.use('/', BaseRoutes);
 app.use('/device_subscriptions', DeviceSubscriptionsRoutes);
 app.use('/ttnmapper_datapoints', TTNMapperDatapointsRoutes);
 app.use('/devices', DevicesRoutes);
