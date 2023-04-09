4f58a5224ac6822426cae257f0102344c082e58e
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-09 22:00:22 +0200

    add pino

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Application } from 'express';
+import pino from 'pino-http';
 
 import BaseRoutes from './routes/base';
 import TTNMapperDatapointsRoutes from './routes/ttnmapper_datapoints';
@@ -7,10 +8,12 @@ import DeviceSubscriptionsRoutes from './routes/device_subscriptions';
 // Fastify instance
 const app: Application = express();
 
+app.use(pino);
+
 // Register Routes
 app.use(BaseRoutes);
-app.use('/ttnmapper_datapoints', TTNMapperDatapointsRoutes);
 app.use('/device_subscriptions', DeviceSubscriptionsRoutes);
+app.use('/ttnmapper_datapoints', TTNMapperDatapointsRoutes);
 
 // Run the server!
 const PORT: number = parseInt(process.env.PORT || '3000');
