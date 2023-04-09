ea6f12d33eb0806439d15a35b1eb7c1fc41a8b29
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-09 22:20:05 +0200

    include pinon logger and fix for nodemon crashes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import express, { Application } from 'express';
-import pino from 'pino-http';
+import pinoHttp from 'pino-http';
+import pino from 'pino';
 
 import BaseRoutes from './routes/base';
 import TTNMapperDatapointsRoutes from './routes/ttnmapper_datapoints';
 import DeviceSubscriptionsRoutes from './routes/device_subscriptions';
 
+// Fix for nodemon crashes
+process.once('SIGUSR2', function () {
+    process.kill(process.pid, 'SIGUSR2');
+});
+
+process.on('SIGINT', function () {
+    // this is only called on ctrl+c, not restart
+    process.kill(process.pid, 'SIGINT');
+});
+
 // Fastify instance
 const app: Application = express();
+const logger = pino();
+const pinoHttpLogger = pinoHttp();
 
-app.use(pino);
+app.use(pinoHttpLogger);
 
 // Register Routes
 app.use(BaseRoutes);
@@ -18,3 +31,5 @@ app.use('/ttnmapper_datapoints', TTNMapperDatapointsRoutes);
 // Run the server!
 const PORT: number = parseInt(process.env.PORT || '3000');
 app.listen(PORT);
+
+logger.info(`Server running on port ${PORT}`);
