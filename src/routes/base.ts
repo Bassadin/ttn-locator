d1bffa9a6b0ea680208daa80efbf27ccec44d8b3
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-10 22:02:38 +0200

    log the number of current subscription on the base route

--- src/routes/base.ts
+++ src/routes/base.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
 // 🏚️ Default Route
 // This is the Default Route of the API
 router.get('/', async (request: Request, response: Response) => {
-    response.send({ message: 'Hello from ttnmapper-reader!' });
+    const numberOfDeviceSubscriptions = await prisma.device_subscription.count();
+    response.send({
+        message: `Hello from ttnmapper-reader!\nCurrent amount of device subscriptions: ${numberOfDeviceSubscriptions}`,
+    });
 });
 
 export default router;
