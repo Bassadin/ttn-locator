7b4e886bb530d116762a3c1de231ce5d9edb1be4
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-10 22:38:22 +0200

    add first jest tests

---
+++ src/routes/base.spec.ts
@@ -0,0 +1,9 @@
+import app from '../app';
+import request from 'supertest';
+
+describe('Test base routes', () => {
+    test('Catch-all route', async () => {
+        const res = await request(app).get('/');
+        expect(res.statusCode).toEqual(200);
+    });
+});
---
+++ src/routes/devices.spec.ts
@@ -0,0 +1,17 @@
+import app from '../app';
+import request from 'supertest';
+
+describe('Test devices routes', () => {
+    test('Get all devices route', async () => {
+        const res = await request(app).get('/devices/');
+        expect(res.statusCode).toEqual(200);
+        // check if data includes at least one device (loris-tracker-hfu)
+        expect(res.body.data).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining({
+                    device_id: 'loris-tracker-hfu',
+                }),
+            ]),
+        );
+    });
+});
