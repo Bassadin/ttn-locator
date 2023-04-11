55eaa8290634f1c7c5ac75940262bb9779e93f9a
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-11 10:05:53 +0200

    use <rootDir>/src in jest config to avoid generated testing js files

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,8 @@ import type { Config } from 'jest';
 const config: Config = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    roots: ['<rootDir>'],
+    // https://stackoverflow.com/a/50863753/3526350
+    roots: ['<rootDir>/src'],
     modulePaths: ['<rootDir>'],
     moduleDirectories: ['node_modules'],
 };
