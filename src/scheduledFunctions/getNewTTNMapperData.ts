ac1a26d4d7423b3f4df40d2ed4cb62d7feff0018
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-17 11:06:35 +0200

    write seperate getNewTTNMapperData method

--- src/scheduledFunctions/getNewTTNMapperData.ts
+++ src/scheduledFunctions/getNewTTNMapperData.ts
@@ -2,12 +2,16 @@ import { CronJob } from 'cron';
 
 export default class GetNewTTNMapperDataCronJob {
     public static initScheduledJob(): void {
-        // https://crontab.guru/#*/5_*_*_*_*
+        // https://crontab.guru/#*/*_*_*_*_*
         const ttnmapperJob = new CronJob('* * * * *', () => {
+            this.getNewTTNMapperData();
             console.log("I'm executed every minute!");
-            // TODO: Implement this with ttnmapper calls
         });
 
         ttnmapperJob.start();
+    }
+
+    private static async getNewTTNMapperData(): Promise<void> {
+        // TODO: Implement this with ttnmapper calls
     }
 }
