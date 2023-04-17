b7a5340bba64fece639cdaf4a39860296697317b
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-17 18:43:30 +0200

    update getting ttn mapper data

--- src/scheduledFunctions/getNewTTNMapperData.ts
+++ src/scheduledFunctions/getNewTTNMapperData.ts
@@ -19,12 +19,58 @@ export default class GetNewTTNMapperDataCronJob {
     public static async getNewTTNMapperDataForSubscribedDevices() {
         const subscribedDevices = await this.prisma.deviceSubscription.findMany();
         logger.info(`Fetching data from TTN Mapper API for ${subscribedDevices.length} subscribed devices`);
-        for (const eachDevice of subscribedDevices) {
-            const apiResponse = await TTNMapperConnection.getNewTTNMapperDataForDevice(eachDevice.deviceId);
-            for (const eachTTNMapperData of apiResponse.body) {
+        for (const eachDeviceSubscription of subscribedDevices) {
+            const apiResponse = await TTNMapperConnection.getNewTTNMapperDataForDevice(eachDeviceSubscription.deviceId);
+            for (const eachTTNMapperAPIDatapoint of apiResponse.body) {
                 logger.debug(
-                    `Inserting new TTNMapper data ${eachTTNMapperData.database_id} for Device ${eachDevice.deviceId}`,
+                    `Inserting new TTNMapper data ${eachTTNMapperAPIDatapoint.database_id} for Device ${eachDeviceSubscription.deviceId}`,
                 );
+
+                const deviceGPSDatapoint = await this.prisma.deviceGPSDatapoint.upsert({
+                    where: {
+                        deviceId_timestamp: {
+                            deviceId: eachDeviceSubscription.deviceId,
+                            timestamp: eachTTNMapperAPIDatapoint.time,
+                        },
+                    },
+                    create: {
+                        device: { connect: { deviceId: eachDeviceSubscription.deviceId } },
+                        timestamp: eachTTNMapperAPIDatapoint.time,
+                        latitude: eachTTNMapperAPIDatapoint.latitude,
+                        longitude: eachTTNMapperAPIDatapoint.longitude,
+                        altitude: eachTTNMapperAPIDatapoint.altitude,
+                        hdop: eachTTNMapperAPIDatapoint.hdop,
+                    },
+                    update: {},
+                });
+
+                const gateway = await this.prisma.gateway.upsert({
+                    where: { gatewayId: eachTTNMapperAPIDatapoint.gateway_id },
+                    create: {
+                        gatewayId: eachTTNMapperAPIDatapoint.gateway_id,
+                        latitude: 0,
+                        longitude: 0,
+                        altitude: 0,
+                    },
+                    update: {
+                        latitude: 0,
+                        longitude: 0,
+                        altitude: 0,
+                    },
+                });
+
+                await this.prisma.ttnMapperDatapoint.upsert({
+                    where: { id: eachTTNMapperAPIDatapoint.database_id },
+                    update: {},
+                    create: {
+                        id: eachTTNMapperAPIDatapoint.database_id,
+                        rssi: eachTTNMapperAPIDatapoint.rssi,
+                        snr: eachTTNMapperAPIDatapoint.snr,
+                        timestamp: eachTTNMapperAPIDatapoint.time,
+                        deviceGPSDatapoint: { connect: { id: deviceGPSDatapoint.id } },
+                        gateway: { connect: { gatewayId: gateway.gatewayId } },
+                    },
+                });
             }
         }
     }
