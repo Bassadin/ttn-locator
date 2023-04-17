2f74e2a9b4e90da0f552d0d8c9e2999d0ecc9649
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-17 15:40:24 +0200

    add function for checking subscribes devices for new data

--- src/scheduledFunctions/getNewTTNMapperData.ts
+++ src/scheduledFunctions/getNewTTNMapperData.ts
@@ -1,17 +1,29 @@
 import { CronJob } from 'cron';
+import { PrismaClient } from '@prisma/client';
+import TTNMapperConnection from '@/helpers/TTNMapperConnection';
+import logger from '@/middleware/logger';
 
 export default class GetNewTTNMapperDataCronJob {
+    private static prisma = new PrismaClient();
+
     public static initScheduledJob(): void {
         // https://crontab.guru/#*/*_*_*_*_*
         const ttnmapperJob = new CronJob('* * * * *', () => {
-            this.getNewTTNMapperData();
+            // TTNMapperConnection.getNewTTNMapperDataForDevice();
             console.log("I'm executed every minute!");
         });
 
         ttnmapperJob.start();
     }
 
-    private static async getNewTTNMapperData(): Promise<void> {
-        // TODO: Implement this with ttnmapper calls
+    public static async getNewTTNMapperDataForSubscribedDevices() {
+        const subscribedDevices = await this.prisma.device_subscription.findMany();
+        logger.info(`Fetching data from TTN Mapper API for ${subscribedDevices.length} subscribed devices`);
+        for (const eachDevice of subscribedDevices) {
+            const apiResponse = await TTNMapperConnection.getNewTTNMapperDataForDevice(eachDevice.device_id);
+            for (const eachTTNMapperData of apiResponse.body) {
+                logger.debug(`Inserting new TTNMapper data ${eachTTNMapperData} for device ${eachDevice.device_id}`);
+            }
+        }
     }
 }
