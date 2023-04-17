ef196be29211450c99ff2a5eb7b3e0b9dd488ff9
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-17 22:24:40 +0200

    change log level to debug

--- src/helpers/GatewayLocationGetter.ts
+++ src/helpers/GatewayLocationGetter.ts
@@ -4,7 +4,7 @@ import superagent from 'superagent';
 
 export default class GatewayLocationGetter {
     public static async getGatewayLocation(gatewayId: string): Promise<GatewayLocation> {
-        logger.info(`Fetching location for gateway ${gatewayId}`);
+        logger.debug(`Fetching location for gateway ${gatewayId}`);
 
         const responseData = await superagent.get(
             `https://mapper.packetbroker.net/api/v2/gateways/netID=000013,tenantID=ttn,id=${gatewayId}`,
