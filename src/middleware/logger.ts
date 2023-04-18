074c2f9c87ddb08e8454ff5669930c79a0ef7d98
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-18 10:18:33 +0200

    use logger with pretty-print if in test mode

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,13 +1,26 @@
 import pino from 'pino';
+import pretty from 'pino-pretty';
 
-const logger = pino();
+let loggerOptions = {};
 
-console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'TEST') {
+    // https://github.com/pinojs/pino-pretty#usage-with-jest
+    loggerOptions = pretty({ sync: true });
+}
+
+const logger = pino(loggerOptions);
+
+logger.info(`Logger initialized, using options: ${JSON.stringify(loggerOptions)}`);
 
 /* istanbul ignore if  */
 if (process.env.NODE_ENV === 'development') {
-    logger.warn('Running in development mode');
     logger.level = 'debug';
 }
+
+if (process.env.NODE_ENV === 'TEST') {
+    logger.level = 'debug';
+}
+
+logger.info(`Logger initialized, running in ${process.env.NODE_ENV} mode`);
 
 export default logger;
