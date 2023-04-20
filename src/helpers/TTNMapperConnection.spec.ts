3659d1ea8146fa3a94859f12273187624a624d4f
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-20 18:44:50 +0200

    restructure test with describe blocks

--- src/helpers/TTNMapperConnection.spec.ts
+++ src/helpers/TTNMapperConnection.spec.ts
@@ -34,6 +34,33 @@ describe('Test TTNMapperConnection class', () => {
         await prisma.deviceGPSDatapoint.deleteMany({ where: dataForExampleGPSDatapoint });
     });
 
+    describe('getNewTTNMapperDataForDevice()', () => {
+        test('Calling getNewTTNMapperDataForDevice() method without device id', async () => {
+            expect(async () => {
+                await TTNMapperConnection.getNewTTNMapperDataForDevice('', new Date(2005, 1, 1));
+            }).rejects.toThrowError();
+        });
+
+        test('Calling getNewTTNMapperDataForDevice() method with a date in the future', async () => {
+            expect(async () => {
+                await TTNMapperConnection.getNewTTNMapperDataForDevice('test_device', new Date(3000, 1, 1));
+            }).rejects.toThrowError();
+        });
+    });
+
+    describe('getTtnMapperApiStartSearchDateForDevice()', () => {
+        test('Calling getTtnMapperApiStartSearchDateForDevice() method with a valid device id', async () => {
+            expect(await TTNMapperConnection.getTtnMapperApiStartSearchDateForDevice('loris-hfu-001')).toBeInstanceOf(
+                Date,
+            );
+        });
+
+        test('Calling getTtnMapperApiStartSearchDateForDevice() method with an empty device id', async () => {
+            expect(async () => {
+                await TTNMapperConnection.getTtnMapperApiStartSearchDateForDevice('');
+            }).rejects.toThrowError();
+        });
+    });
     test('Calling getNewTTNMapperDataForDevice() method without device id', async () => {
         expect(async () => {
             await TTNMapperConnection.getNewTTNMapperDataForDevice('', new Date(2005, 1, 1));
