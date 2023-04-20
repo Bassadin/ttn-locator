992445d5b80fe6861b1e5ea63396dac88520267a
Bastian Hodapp <bastianhodapp@gmail.com>
2023-04-20 14:42:24 +0200

    add endpoint for getting ttnmapper datapoints for specific gps datapoint with location values

--- src/routes/device_gps_datapoints.spec.ts
+++ src/routes/device_gps_datapoints.spec.ts
@@ -10,17 +10,25 @@ describe('Test device_gps_datapoints routes', () => {
         expect(response.body.data.length).toBeGreaterThan(0);
     });
 
-    test('Get all ttnmapper_datapoints for a device_gps_datapoint route', async () => {
-        const response = await request(app).get(`/device_gps_datapoints/1/ttnmapper_datapoints`);
+    test('Get all device_gps_datapoints with min_ttnmapper_datapoints route', async () => {
+        const response = await request(app).get(`/device_gps_datapoints?min_ttnmapper_datapoints=3`);
+
+        expect(response.statusCode).toEqual(200);
+    });
+
+    test('ttnmapper_datapoints_with_gateway_locations route with valid id', async () => {
+        const response = await request(app).get(`/device_gps_datapoints/1/ttnmapper_datapoints_with_gateway_locations`);
 
         expect(response.statusCode).toEqual(200);
         // check if data array is at least 1 long
         expect(response.body.data.length).toEqual(1);
     });
 
-    test('Get all device_gps_datapoints with min_ttnmapper_datapoints route', async () => {
-        const response = await request(app).get(`/device_gps_datapoints?min_ttnmapper_datapoints=3`);
+    test('ttnmapper_datapoints_with_gateway_locations route with invalid id', async () => {
+        const response = await request(app).get(
+            `/device_gps_datapoints/-123456789/ttnmapper_datapoints_with_gateway_locations`,
+        );
 
-        expect(response.statusCode).toEqual(200);
+        expect(response.statusCode).toEqual(404);
     });
 });
--- src/routes/device_gps_datapoints.ts
+++ src/routes/device_gps_datapoints.ts
@@ -34,16 +34,40 @@ router.get('/', async (request: Request, response: Response) => {
 });
 
 // Get all ttnmapper datapoints for a device GPS datapoint
-router.get('/:id/ttnmapper_datapoints', async (request: Request, response: Response) => {
-    const ttnMapperDatapoints = await prisma.deviceGPSDatapoint
-        .findUnique({
-            where: {
-                id: Number(request.params.id),
+router.get('/:id/ttnmapper_datapoints_with_gateway_locations', async (request: Request, response: Response) => {
+    const deviceGPSDatapoints = await prisma.deviceGPSDatapoint.findUnique({
+        where: {
+            id: Number(request.params.id),
+        },
+        select: {
+            ttnMapperDatapoints: {
+                select: {
+                    id: true,
+                    rssi: true,
+                    snr: true,
+                    timestamp: true,
+                    gateway: {
+                        select: {
+                            gatewayId: true,
+                            latitude: true,
+                            longitude: true,
+                            altitude: true,
+                        },
+                    },
+                },
             },
-        })
-        .ttnMapperDatapoints();
+        },
+    });
+
+    if (!deviceGPSDatapoints) {
+        response.status(404).send({
+            error: 'Device GPS datapoint not found',
+        });
+        return;
+    }
+
     response.send({
-        data: ttnMapperDatapoints,
+        data: deviceGPSDatapoints.ttnMapperDatapoints,
     });
 });
 
