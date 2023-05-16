b38a461fd45580b37e79b047121aa5e8eba3af96
Bastian Hodapp <bastianhodapp@gmail.com>
2023-05-16 11:39:42 +0200

    add endpoint to get single ttnmapper datapoint

--- src/routes/ttnmapper_datapoints.ts
+++ src/routes/ttnmapper_datapoints.ts
@@ -11,4 +11,20 @@ router.get('/', async (request: Request, response: Response) => {
     response.send({ message: 'success', data: ttnmapper_datapoints });
 });
 
+// Get single TTNMapper datapoint
+router.get('/:id', async (request: Request, response: Response) => {
+    const ttnmapper_datapoint = await prisma.ttnMapperDatapoint.findUnique({
+        where: {
+            id: Number(request.params.id),
+        },
+    });
+
+    if (!ttnmapper_datapoint) {
+        response.status(404).send({ message: 'TTNMapper datapoint not found' });
+        return;
+    }
+
+    response.send({ message: 'success', data: ttnmapper_datapoint });
+});
+
 export default router;
