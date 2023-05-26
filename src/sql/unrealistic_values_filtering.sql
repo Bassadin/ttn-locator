c0543a58f1d3960f8d13237ba3ccb82509f0e9b4
Bastian Hodapp <bastianhodapp@gmail.com>
2023-05-26 13:07:10 +0200

    improve sql query

--- src/sql/unrealistic_values_filtering.sql
+++ src/sql/unrealistic_values_filtering.sql
@@ -17,6 +17,16 @@ WITH distance_calculations AS (
             ORDER BY
                 timestamp
         ) AS prev_longitude,
+        LAG(altitude) OVER (
+            PARTITION BY "deviceId"
+            ORDER BY
+                timestamp
+        ) AS prev_altitude,
+        LAG(hdop) OVER (
+            PARTITION BY "deviceId"
+            ORDER BY
+                timestamp
+        ) AS prev_hdop,
         LAG(timestamp) OVER (
             PARTITION BY "deviceId"
             ORDER BY
@@ -28,6 +38,7 @@ WITH distance_calculations AS (
 distance_filtered AS (
     SELECT
         *,
+        -- Calculate speed in meters per second between previous and current point
         CASE
             WHEN prev_latitude IS NOT NULL
             AND prev_longitude IS NOT NULL
@@ -40,7 +51,19 @@ distance_filtered AS (
                     (timestamp - prev_timestamp)
             )
             ELSE NULL
-        END AS speed
+        END AS speed,
+        -- Check if current point data is the same as in the previous point
+        CASE
+            WHEN prev_latitude IS NOT NULL
+            AND prev_longitude IS NOT NULL
+            AND prev_altitude IS NOT NULL
+            AND prev_hdop IS NOT NULL
+            AND prev_timestamp IS NOT NULL THEN latitude = prev_latitude
+            AND longitude = prev_longitude
+            AND altitude = prev_altitude
+            AND hdop = prev_hdop
+            ELSE NULL
+        END AS is_same_as_previous
     FROM
         distance_calculations
 )
@@ -49,4 +72,5 @@ SELECT
 FROM
     distance_filtered
 WHERE
-    OR speed > 40 -- maximum speed in meters per second
+    speed > 40 -- maximum speed in meters per second
+    OR is_same_as_previous = true
